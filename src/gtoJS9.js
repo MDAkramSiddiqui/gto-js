7af31ff41d9c4fc6109a7de180c9ff9564eb9b9e
Mohammad Akram <40524561+MDAkramSiddiqui@users.noreply.github.com>
2021-08-31 17:09:07 +0530

    ADDED :: JS9

---
+++ src/gtoJS9.js
@@ -0,0 +1,24 @@
+let user = {
+  name: "John",
+  surname: "Smith",
+
+  set fullName(value) {
+    [this.name, this.surname] = value.split(" ");
+  },
+
+  get fullName() {
+    return `${this.name} ${this.surname}`;
+  },
+};
+
+let admin = {
+  __proto__: user,
+  isAdmin: true,
+};
+
+alert(admin.fullName);
+
+admin.fullName = "Alice Cooper";
+
+alert(admin.fullName);
+alert(user.fullName);
